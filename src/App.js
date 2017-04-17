e47ad2438a63edd03a9f330664ee8fdcbe1ad322
zuhrasofyan <zuhrasofyan@gmail.com>
2017-04-17 11:42:02 +0700

    .

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button className="btn btn-primary">dfdfdf</button>
+        <button className="btn btn-primary">heyho</button>
         <img src={sam} alt=""/>
       </div>
     );
