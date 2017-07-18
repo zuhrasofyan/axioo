67c0217840c73d3ba2dedaeb5fe1ab4c38312027
zuhrasofyan <zuhrasofyan@gmail.com>
2017-07-18 16:35:27 +0700

    Add list object to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,6 +3,26 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Body from './Body';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+
 class Layout extends Component {
   constructor(props) {
     super(props);
@@ -19,6 +39,18 @@ class Layout extends Component {
       <div>
         <Header />
         <Body changeTitle={this.changeTitle} title={this.state.title}/>
+        { list.map(function(item){
+          return (
+            <div key={ item.objectID }>
+              <span>
+                <a href={ item.url }>{ item.title }</a> &nbsp;
+              </span>
+              <span>{ item.author } </span>
+              <span>{ item.num_comments } </span>
+              <span>{ item.points } </span>
+            </div>
+          );
+        }) }
       </div>
     );
   }
